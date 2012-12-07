2f1355b1d22ef73b4e6922e3e49ebbae78a1b323
Oz Nahum <nahumoz@gmail.com>
2012-12-07 12:49:19 +0100

    add disquss

--- blogit.py
+++ blogit.py
@@ -106,7 +106,22 @@ GLOBAL_TEMPLATE_CONTEXT = {
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
-</script>"""
+</script>""",
+    'disquss' : """<div id="disqus_thread"></div>
+        <script type="text/javascript">
+            /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+            var disqus_shortname = 'oz123githubcom'; // required: replace example with your forum shortname
+
+            /* * * DON'T EDIT BELOW THIS LINE * * */
+            (function() {
+                var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+            })();
+        </script>
+        <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+        <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+    """
 }
 
 KINDS = {
@@ -213,7 +228,7 @@ class Entry(object):
                 (
                     self.published.strftime("%Y-%m-%d"),
                     self.permalink,
-		    GLOBAL_TEMPLATE_CONTEXT["site_url"]
+            GLOBAL_TEMPLATE_CONTEXT["site_url"]
                 )
 
 
