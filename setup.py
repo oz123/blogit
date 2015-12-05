e05b4b8a2b819593d83eb3762c0989a04b8e9c63
Oz N Tiram <nahumoz@gmail.com>
2015-12-05 11:31:31 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='blogit2',
+      version='0.0.1',
+      license="GNU GPL",
+          scripts=['blogit2.py']
+      )
