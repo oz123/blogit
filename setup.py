c7b10b31441885ba6ca31f91b1d7f76310daa31e
Oz N Tiram <nahumoz@gmail.com>
2016-03-02 23:21:20 +0100

    update metadata

--- setup.py
+++ setup.py
@@ -4,13 +4,26 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='blogit',
-      version='0.0.1',
+      version='0.1',
       license="GNU GPL",
       packages=find_packages(exclude=['tests']),
       install_requires=['Jinja2', 'markdown2', 'tinydb', 'pygments'],
       tests_require=['pytest', 'beautifulsoup4'],
       entry_points={
               'console_scripts': ['blogit = blogit.blogit:main']
-          }
+          },
+
+      classifiers=['Environment :: Console',
+                   'Intended Audience :: End Users/Desktop',
+                   'Intended Audience :: Developers',
+                   ('License :: OSI Approved :: GNU General Public License'
+                    ' v3 or later (GPLv3+)'),
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   ],
       )
 
